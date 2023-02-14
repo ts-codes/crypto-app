7a3f5971d083ea91c994f00f07832dac6c91af45
Thomas Salmon <83253291+ts-codes@users.noreply.github.com>
2023-02-14 20:06:33 +0000

    added Hashrouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ColorModeScript } from '@chakra-ui/react';
 import theme from './theme';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
